c361fc40e728bf765a136481dc71210cf115502b
oodafeisrael <oodafeisrael@yahoo.com>
2024-01-18 04:47:31 +0100

    new update

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,10 +2,11 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import pycodestyle
 
 
 class test_City(test_basemodel):
-    """ """
+    """Represents the tests for the City model """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -22,3 +23,30 @@ class test_City(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    def test_attributes_City(self):
+        """chekcing if City have attributes"""
+        self.assertTrue('id' in self.city.__dict__)
+        self.assertTrue('created_at' in self.city.__dict__)
+        self.assertTrue('updated_at' in self.city.__dict__)
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+
+    def test_attribute_types_City(self):
+        """test attribute type for City"""
+        self.assertEqual(type(self.city.name), str)
+        self.assertEqual(type(self.city.state_id), str)
+
+    def test_save_City(self):
+        """test if the save works"""
+        self.city.save()
+        self.assertNotEqual(self.city.created_at,
+                            self.city.updated_at)
+
+    def test_to_dict_City(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.city), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
